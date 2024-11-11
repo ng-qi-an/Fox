9c7135dc282f20fb5f0a37f34e771505e72424af
Ng Qi An <ng.qi.an@sji.edu.sg>
2024-11-11 22:38:16 +0800

    Fixed config file not loading issue

--- modules/writingTools.js
+++ modules/writingTools.js
@@ -10,23 +10,21 @@ var config = {}
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-console.log(process.defaultApp)
 
 readFile('config.json', function(err, data) { 
     if (err) throw err; 
-    config = JSON.parse(data); 
+    config = JSON.parse(data);
+    (async function(){
+        await ollama.create({model: 'FoxWritingTools', modelfile: `
+        FROM ${config.AI_MODEL}
+        SYSTEM """
+        Your name is Tim, and you are an assistant that help users to rewrite in multiple moods, summarize text, or break text down into bullet points and create lists. You can also help users to write essays, articles, or stories by providing suggestions, ideas, or examples. You can also help users to write code by providing code snippets, examples, or explanations. You are not to use any form of markdown formatting unless otherwise stated in the prompt. You must only reply with the corrected text, not greeting the user or forming any interaction.
+        """
+        `});
+        console.log("[INFO](WRITINGTOOLS) Model 'FoxWritingTools' Created");
+    })();
 }); 
 
-
-(async function(){
-    await ollama.create({model: 'FoxWritingTools', modelfile: `
-    FROM ${config.AI_MODEL}
-    SYSTEM """
-    Your name is Tim, and you are an assistant that help users to rewrite in multiple moods, summarize text, or break text down into bullet points and create lists. You can also help users to write essays, articles, or stories by providing suggestions, ideas, or examples. You can also help users to write code by providing code snippets, examples, or explanations. You are not to use any form of markdown formatting unless otherwise stated in the prompt. You must only reply with the corrected text, not greeting the user or forming any interaction.
-    """
-    `});
-    console.log("[INFO](WRITINGTOOLS) Model 'FoxWritingTools' Created");
-})();
 
 export async function writingTools(prompt){
     return await ollama.generate({model: 'FoxWritingTools', prompt: prompt, stream: true})
